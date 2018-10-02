ed769fe7b6fa8eaa02c2201ec60d1435a08631e1
karanam sai phani teja <43602536+saiphani1702@users.noreply.github.com>
2018-10-02 17:00:23 +0530

    Update assign.js

--- assign.js
+++ assign.js
@@ -1,24 +1,24 @@
-console.log("Recipe Details");
-console.log("---------------");
-var recipeName="creamy squash linguine";
-var preparationTime=45;
-var recipeBy="Jimmy Cutter";
-var isVegetarian=true;
-var calories=223.7;
-console.log("Recipe Name: "+recipeName);
-console.log("Preparation Time: "+preparationTime);
-console.log("Recipe By: "+recipeBy);
-console.log("Is Vegetarian: "+isVegetarian);
-console.log("Calories: "+calories);
-console.log("Recipe Deatils after Update");
-console.log("-----------------------------");
-var recipeName="creamy squash linguine";
-var preparationTime=60;
-var recipeBy="Jimmy Cutter George";
-var isVegetarian=true;
-var calories=223.7;
-console.log("Recipe Name: "+recipeName);
-console.log("Preparation Time: "+preparationTime);
-console.log("Recipe By: "+recipeBy);
-console.log("Is Vegetarian: "+isVegetarian);
-console.log("Calories: "+calories);
+console.log("Recipe Details");//displays output to the console
+console.log("---------------");//displays output to the console
+var recipeName="creamy squash linguine";//initializing variable
+var preparationTime=45;//initializing variable
+var recipeBy="Jimmy Cutter";//initializing variable
+var isVegetarian=true;//initializing variable
+var calories=223.7;//initializing variable
+console.log("Recipe Name: "+recipeName);//displays output to the console
+console.log("Preparation Time: "+preparationTime);//displays output to the console
+console.log("Recipe By: "+recipeBy);//displays output to the console
+console.log("Is Vegetarian: "+isVegetarian);//displays output to the console
+console.log("Calories: "+calories);//displays output to the console
+console.log("Recipe Deatils after Update");//displays output to the console
+console.log("-----------------------------");//displays output to the console
+var recipeName="creamy squash linguine";//initializing variable
+var preparationTime=60;//initializing variable
+var recipeBy="Jimmy Cutter George";//initializing variable
+var isVegetarian=true;//initializing variable
+var calories=223.7;//initializing variable
+console.log("Recipe Name: "+recipeName);//displays output to the console
+console.log("Preparation Time: "+preparationTime);//displays output to the console
+console.log("Recipe By: "+recipeBy);//displays output to the console
+console.log("Is Vegetarian: "+isVegetarian);//displays output to the console
+console.log("Calories: "+calories);//displays output to the console
